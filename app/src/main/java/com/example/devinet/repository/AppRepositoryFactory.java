2bee4360b552780bfb5abb1997da7458c767bea0
Nessgyver <dam.rouxel@gmail.com>
2020-08-10 14:49:43 +0200

    implemented AppRepositoryFactory

--- app/src/main/java/com/example/devinet/repository/AppRepositoryFactory.java
+++ app/src/main/java/com/example/devinet/repository/AppRepositoryFactory.java
@@ -1,5 +1,18 @@
 package com.example.devinet.repository;
 
-public class AppRepositoryFactory {
+import android.content.Context;
+
+import com.example.devinet.repository.bdd.CategorieBddRepository;
+import com.example.devinet.repository.bdd.MotBddRepository;
+
+public abstract class AppRepositoryFactory {
+
+    public static ICategorieRepository getCategorieRepository(Context context){
+        return new CategorieBddRepository(context);
+    }
+
+    public static IMotRepository getMotRepository(Context context){
+        return new MotBddRepository(context);
+    }
 
 }
