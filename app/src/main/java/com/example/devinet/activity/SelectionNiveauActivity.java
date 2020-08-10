a161065807421616d179f473fb5e8c455b19c2ba
Nessgyver <dam.rouxel@gmail.com>
2020-08-10 16:53:40 +0200

    created and implemented home page + SelectionNiveauActivity

---
+++ app/src/main/java/com/example/devinet/activity/SelectionNiveauActivity.java
@@ -0,0 +1,17 @@
+package com.example.devinet.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.devinet.R;
+
+public class SelectionNiveauActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_niveau);
+
+    }
+}
