b96fba8a8d31d0ee775f711f8984789a9576b7c0
dam.rouxel@gmail.com <GitKr@ken>
2020-08-11 16:39:01 +0200

    created style_ligne_niveau.xml

--- app/src/main/java/com/example/devinet/activity/adapter/CategorieAdapter.java
+++ app/src/main/java/com/example/devinet/activity/adapter/CategorieAdapter.java
@@ -1,4 +1,16 @@
 package com.example.devinet.activity.adapter;
 
-public class CategorieAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import com.example.devinet.bo.Categorie;
+
+import java.util.List;
+
+public class CategorieAdapter extends ArrayAdapter<Categorie> {
+    public CategorieAdapter(@NonNull Context context, int resource, @NonNull List<Categorie> objects) {
+        super(context, resource, objects);
+    }
 }
