b2962f5e05bdba41cf850da7ba148fefe395830c
Nessgyver <dam.rouxel@gmail.com>
2020-08-11 12:06:46 +0200

    preferences are now stored in a file

--- app/src/main/java/com/example/devinet/activity/MesPreferencesActivity.java
+++ app/src/main/java/com/example/devinet/activity/MesPreferencesActivity.java
@@ -4,18 +4,39 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Switch;
 
 import com.example.devinet.R;
 
 public class MesPreferencesActivity extends AppCompatActivity {
 
+    public static final String CLE_SON = "son";
+    public static final String CLE_VIBRATION  = "vibration";
+    public static final String NOM_FICHIER  = "parametres";
+
+    private Switch sb_son = null;
+    private Switch sb_vibration = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mes_preferences);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sb_son = findViewById(R.id.sb_son);
+        sb_vibration = findViewById(R.id.sb_vibration);
+
+        SharedPreferences sp = getSharedPreferences(NOM_FICHIER, MODE_PRIVATE);
+        sb_son.setChecked(sp.getBoolean(CLE_SON, true));
+        sb_vibration.setChecked(sp.getBoolean(CLE_VIBRATION, true));
     }
 
     @Override
@@ -48,5 +69,11 @@ public class MesPreferencesActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
+        SharedPreferences sp = getSharedPreferences(NOM_FICHIER, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean(CLE_SON, sb_son.isChecked());
+        editor.putBoolean(CLE_VIBRATION, sb_vibration.isChecked());
+        editor.commit();
     }
 }
