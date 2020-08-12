e212aee2d106bb27c5716eda1e9551fc06a346f8
Nessgyver <dam.rouxel@gmail.com>
2020-08-12 15:28:30 +0200

    implemented activity_trouver_mot.xml

--- app/src/main/java/com/example/devinet/activity/TrouverMotActivity.java
+++ app/src/main/java/com/example/devinet/activity/TrouverMotActivity.java
@@ -3,6 +3,8 @@ package com.example.devinet.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.devinet.R;
 
@@ -12,5 +14,9 @@ public class TrouverMotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trouver_mot);
+    }
+
+    public void onClickAjouterLettre(View view) {
+        Toast.makeText(this, view.toString(), Toast.LENGTH_SHORT).show();
     }
 }
