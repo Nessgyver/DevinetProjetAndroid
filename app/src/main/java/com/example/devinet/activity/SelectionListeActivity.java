ac9a05b21db3deafdfb878f33a47bbd438f836bd
Nessgyver <dam.rouxel@gmail.com>
2020-08-12 14:37:20 +0200

    implemented SelectionListeActivity

--- app/src/main/java/com/example/devinet/activity/SelectionListeActivity.java
+++ app/src/main/java/com/example/devinet/activity/SelectionListeActivity.java
@@ -1,16 +1,68 @@
 package com.example.devinet.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.devinet.R;
+import com.example.devinet.activity.adapter.CategorieAdapter;
+import com.example.devinet.activity.adapter.ListeMotAdapter;
+import com.example.devinet.bo.Categorie;
+import com.example.devinet.view_model.CategorieViewModel;
+import com.example.devinet.view_model.MotViewModel;
+
+import java.util.List;
 
 public class SelectionListeActivity extends AppCompatActivity {
+
+    private CategorieViewModel cvm;
+    private MotViewModel mvm;
+
+    private LiveData<List<Categorie>> observerCategories;
+    private ListView listeMot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection_liste);
+
+        cvm = ViewModelProviders.of(this).get(CategorieViewModel.class);
+        mvm = ViewModelProviders.of(this).get(MotViewModel.class);
+
+        listeMot = findViewById(R.id.lv_liste);
+
+        listeMot.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Categorie categorie = (Categorie) listeMot.getItemAtPosition(i);
+                Intent intent  = new Intent(SelectionListeActivity.this, TrouverMotActivity.class);
+                intent.putExtra("idCategorie", categorie.getId());
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        observerCategories = cvm.get();
+
+        Intent intent = getIntent();
+        final int choix = intent.getIntExtra("idCategorie", 1);
+
+        observerCategories.observe(this, new Observer<List<Categorie>>() {
+            @Override
+            public void onChanged(List<Categorie> categories) {
+                ListeMotAdapter adapter = new ListeMotAdapter(SelectionListeActivity.this, R.layout.style_ligne_niveau, categories);
+                listeMot.setAdapter(adapter);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/devinet/activity/adapter/ListeMotAdapter.java
+++ app/src/main/java/com/example/devinet/activity/adapter/ListeMotAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -23,11 +24,11 @@ import com.example.devinet.view_model.MotViewModel;
 
 import java.util.List;
 
-public class CategorieAdapter extends ArrayAdapter<Categorie> {
+public class ListeMotAdapter extends ArrayAdapter<Categorie> {
 
     private Context context;
 
-    public CategorieAdapter(@NonNull Context context, int resource, @NonNull List<Categorie> objects) {
+    public ListeMotAdapter(@NonNull Context context, int resource, @NonNull List<Categorie> objects) {
         super(context, resource, objects);
         this.context = context;
     }
@@ -36,16 +37,18 @@ public class CategorieAdapter extends ArrayAdapter<Categorie> {
     @Override
     public View getView(int position, @Nullable View nouvelleLigne, @NonNull ViewGroup parent) {
 
-        MotViewModel mvm = ViewModelProviders.of((FragmentActivity) context).get(MotViewModel.class);
+        MotViewModel mvm = ViewModelProviders.of((FragmentActivity)context).get(MotViewModel.class);
         Categorie categorie = getItem(position);
 
-        if(nouvelleLigne == null){
-            LayoutInflater li = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (nouvelleLigne == null){
+            LayoutInflater li = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             nouvelleLigne = li.inflate(R.layout.style_ligne_niveau, parent, false);
         }
 
-        TextView tvNiveau = nouvelleLigne.findViewById(R.id.tv_ligne_niveau);
-        final ProgressBar pbNiveau = nouvelleLigne.findViewById(R.id.pb_ligne_niveau);
+        TextView tvListe = nouvelleLigne.findViewById(R.id.tv_ligne_niveau);
+        final ProgressBar pbListe = nouvelleLigne.findViewById(R.id.pb_ligne_niveau);
+        Button btnReinit = nouvelleLigne.findViewById(R.id.btn_reinitialiser);
+
         LiveData<List<Mot>> listeMotsCategorie = mvm.get(categorie.getId());
 
         listeMotsCategorie.observe((LifecycleOwner) this.getContext(), new Observer<List<Mot>>() {
@@ -58,14 +61,14 @@ public class CategorieAdapter extends ArrayAdapter<Categorie> {
                             motOk ++;
                         }
                     }
-                    pbNiveau.setProgress((int)(motOk/mots.size()*100));
+                    pbListe.setProgress((int)(motOk/mots.size()*100));
                 }
             }
         });
 
-        tvNiveau.setText(categorie.getNom());
+        tvListe.setText("Liste" + (position + 1) );
+        btnReinit.setVisibility(View.INVISIBLE);
 
-
-        return  nouvelleLigne;
+        return nouvelleLigne;
     }
 }
